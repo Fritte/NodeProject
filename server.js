555d83b18504ed85e428c4ad7d9128b9d5e9f9ac
Arved Baus <arved.baus@gmail.com>
2015-03-22 13:18:01 +0100

    Server updates data if something has changed

--- server.js
+++ server.js
@@ -6,8 +6,12 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var equal = require('deep-equal');
 var path = require('path');
 var request = require('request');
+
+// Server keeps track of the last JSON get
+var lastRequestJSON;
 
 var apiAdresses = require('./lib/apiAdresses');
 var constants = require('./lib/constants');
@@ -19,42 +23,43 @@ var currentMatchDay;
 var serverPort = process.argv[2] || 3000;
 var fakeServerAddress = "http://localhost:3001";
 
-request(fakeServerAddress+apiAdresses.CURRENT_MATCHDAY, function(error, response, body){
+request(fakeServerAddress + apiAdresses.CURRENT_MATCHDAY, function (error, response, body) {
     if (!error && response.statusCode == 200) {
         var responseFromServer = JSON.parse(response.body);
         console.log(responseFromServer.GroupName);
-        currentMatchDay = responseFromServer;
-    } else{
+        currentMatchDay = responseFromServer.GroupOrderId;
+    } else {
         console.log(error);
         console.log('Error connecting to fake server \n Unable to fetch current match day');
     }
 });
 
-/**
- * Refreshes the match results every x seconds
- * Send them to client if something has changed
- */
-setInterval(function(){
-}, constants.REFRESH_RATE_IN_SECONDS * 1000);
-
 app.use(express.static(__dirname + '/public'));
 
-app.get('/', function(req, res){
+app.get('/', function (req, res) {
     res.sendfile('index.html');
 });
 
-io.on('connection', function(socket){
-    socket.on('get matches', function(){
-        //get match data from fake API
+io.on('connection', function (socket) {
 
-        //TODO: Ajax get to fake server
-        //Pass currently running matches with event 'send matches'
+    var resultsInterval = setInterval(function () {
+        request(fakeServerAddress + apiAdresses.MATCH_DATA_OF_DAY + currentMatchDay, function (error, response, body) {
+            var responseFromServer = JSON.parse(response.body);
 
-        var matchId = "someID";
-        io.emit('send matches', matchId);
+            if(!equal(responseFromServer, lastRequestJSON)){
+                console.log(response.body);
+                lastRequestJSON = responseFromServer;
+                //TODO: send Data
+
+            }
+        });
+    }, constants.REFRESH_RATE_IN_SECONDS * 1000);
+
+    socket.on('disconnect', function () {
+        clearInterval(resultsInterval);
     });
 });
 
-http.listen(serverPort, function(){
-    console.log('listening on port:'+serverPort);
+http.listen(serverPort, function () {
+    console.log('listening on port:' + serverPort);
 });
